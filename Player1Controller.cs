97ecb8fc15450f6b31101e0ed8f409b8b2f9832d
ZachMarten <zachary.marten@cuw.edu>
2024-02-14 09:36:59 -0600

    Added Scene2

--- Player1Controller.cs
+++ Player1Controller.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEditor.SceneManagement;
 
 public class Player1Controller : MonoBehaviour
 {
@@ -9,6 +10,7 @@ public class Player1Controller : MonoBehaviour
     public TextMeshPro playerInfo;
     public GameObject destinationGO;
     public float speed = 0;
+    public GameObject EntryGO;
 
 
     // Start is called before the first frame update
@@ -25,12 +27,20 @@ public class Player1Controller : MonoBehaviour
         playerInfo.text = this.thePlayer.getName() + " -> " + this.thePlayer.getHP();
 
     }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        EditorSceneManager.LoadScene("Scene2");
+       
+    }
+
     // Update is called once per frame
     void Update()
     {
         this.gameObject.transform.position = Vector3.MoveTowards(this.gameObject.transform.position, 
                                                     this.destinationGO.transform.position, speed);
-               
+        this.gameObject.transform.position = Vector3.MoveTowards(this.gameObject.transform.position, 
+                                                    this.EntryGO.transform.position, speed);
     }
 }
 
